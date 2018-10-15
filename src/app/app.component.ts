482f9a1c0abccf8d722819a041f51b0e4d078830
DESKTOP-LLU9OKL\FelipeGerolomo <felipegerolomo@gmail.com>
2018-10-15 17:58:49 -0300

    Criada a pagina do produto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
+import { ProdutoPage } from '../pages/produto/produto';
 
 @Component({
   templateUrl: 'app.html'
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ProdutoPage } from '../produto/produto';
 
 @Component({
   selector: 'page-home',
@@ -20,6 +21,10 @@ export class HomePage {
       { id: 1, produto: 'iPhone X', preco: 'R$ 3.000,00', img: 'assets/imgs/p2.jpg' },
       { id: 2, produto: 'Nitendo Switch', preco: 'R$ 2.000,00', img: 'assets/imgs/p1.jpg' },
     ]
+  }
+
+  goToProduto(id) {
+    this.navCtrl.push(ProdutoPage)
   }
 
 }
