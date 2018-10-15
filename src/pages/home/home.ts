e13387b03c6da2f05ad416ce326e70645a2767c8
DESKTOP-LLU9OKL\FelipeGerolomo <felipegerolomo@gmail.com>
2018-10-15 15:43:02 -0300

    tela de lista de produtos criado

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,20 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  produtos: any;
   constructor(public navCtrl: NavController) {
-
+    this.produtos = [
+      { id: 1, produto: 'iPhone X', preco: 'R$ 3.000,00', img: 'assets/imgs/p2.jpg' },
+      { id: 2, produto: 'Nitendo Switch', preco: 'R$ 2.000,00', img: 'assets/imgs/p1.jpg' },
+      { id: 1, produto: 'iPhone X', preco: 'R$ 3.000,00', img: 'assets/imgs/p2.jpg' },
+      { id: 2, produto: 'Nitendo Switch', preco: 'R$ 2.000,00', img: 'assets/imgs/p1.jpg' },
+      { id: 1, produto: 'iPhone X', preco: 'R$ 3.000,00', img: 'assets/imgs/p2.jpg' },
+      { id: 2, produto: 'Nitendo Switch', preco: 'R$ 2.000,00', img: 'assets/imgs/p1.jpg' },
+      { id: 1, produto: 'iPhone X', preco: 'R$ 3.000,00', img: 'assets/imgs/p2.jpg' },
+      { id: 2, produto: 'Nitendo Switch', preco: 'R$ 2.000,00', img: 'assets/imgs/p1.jpg' },
+      { id: 1, produto: 'iPhone X', preco: 'R$ 3.000,00', img: 'assets/imgs/p2.jpg' },
+      { id: 2, produto: 'Nitendo Switch', preco: 'R$ 2.000,00', img: 'assets/imgs/p1.jpg' },
+    ]
   }
 
 }
